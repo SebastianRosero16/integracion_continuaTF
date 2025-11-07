676151b6000500d27ac0c9cf5a81ef08947a1ddd
SebastianRosero16 <sebastian.manchabajo@campusucc.edu.co>
2025-11-07 00:14:13 -0500

    test js a cjs

---
+++ jest.config.cjs
@@ -0,0 +1,29 @@
+/* eslint-env node */
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      { tsconfig: 'tsconfig.json' },
+    ],
+  },
+  // Solo toma tests en estas 3 carpetas
+  testMatch: [
+    '<rootDir>/src/__tests__/Matematicas/**/*.test.ts?(x)',
+    '<rootDir>/src/__tests__/CienciasNaturales/**/*.test.ts?(x)',
+    '<rootDir>/src/__tests__/Arte/**/*.test.ts?(x)',
+  ],
+  collectCoverageFrom: [
+    'src/**/Matematicas/**/*.{ts,tsx}',
+    'src/**/CienciasNaturales/**/*.{ts,tsx}',
+    'src/**/Arte/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+  ],
+};
